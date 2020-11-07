a47754b2ac04be12a63064a6d84978e858243b09
Glitch (cosmic-perpetual-dog) <none>
2020-11-07 09:36:14 +0000

    💇🏎 Checkpoint ./myApp.js:24744670/267

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
-console.log("Hello World")
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
+app.use(express.static(__dirname + '/public'))
+
+app.get('/json', (req, res))
 
 
 
