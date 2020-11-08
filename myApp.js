d2c6e7f73f545b0d3a8085f4d2448d8b4c3bb68a
Glitch (cosmic-perpetual-dog) <none>
2020-11-08 12:54:18 +0000

    🐮🏔 Checkpoint ./myApp.js:24744670/309

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
 const personSchema = new mongoose.Schema({
   name: {type: String, required: true},
   age: Number,
@@ -11,7 +12,14 @@ const personSchema = new mongoose.Schema({
 let Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  const newPerson = new Person({name: 'Pete', age: 22, favoriteFoods: ['pizza', 'ice cream']})
+  newPerson.save((err, data) => {
+    if (err) {
+      done(err)
+    } else {
+      done(null, data)
+    }
+  })
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
